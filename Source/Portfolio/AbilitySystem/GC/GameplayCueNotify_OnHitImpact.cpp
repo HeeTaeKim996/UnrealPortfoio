c5406e8c5ed7f58a3d04b02744a1e3f2a335a9bc
HeeTaeKim996 <ttiiger1@naver.com>
2025-12-09 15:35:32 +0900

    GC_OnImpact_Complete

--- Source/Portfolio/AbilitySystem/GC/GameplayCueNotify_OnHitImpact.cpp
+++ Source/Portfolio/AbilitySystem/GC/GameplayCueNotify_OnHitImpact.cpp
@@ -9,15 +9,12 @@
 
 UGameplayCueNotify_OnHitImpact::UGameplayCueNotify_OnHitImpact()
 {
-	GameplayCueTag = R1Tags::GameplayCue_HitReact_HitImpact;
+
 }
 
 void UGameplayCueNotify_OnHitImpact::HandleGameplayCue(AActor* Target, EGameplayCueEvent::Type EventType, 
 	const FGameplayCueParameters& Parameters)
 {
-	DebugMessage(TEXT("GC_HitImpact : Impact Check"));
-
-
 	Super::HandleGameplayCue(Target, EventType, Parameters);
 	if (EventType != EGameplayCueEvent::Executed || Target == nullptr) return;
 
@@ -30,7 +27,10 @@ void UGameplayCueNotify_OnHitImpact::HandleGameplayCue(AActor* Target, EGameplay
 	if (HitFx)
 	{
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, HitFx, HItResult->Location,
-			HItResult->ImpactNormal.ToOrientationRotator(), FVector(1.f, 1.f, 1.f), true, true,
+			(-HItResult->ImpactNormal).ToOrientationRotator(), FVector(1.f, 1.f, 1.f), true, true,
 			ENCPoolMethod::None, true);
 	}
+
+	DebugMessage(FString::Printf(TEXT("GC_HitImpact [%f, %f, %f]"), HItResult->ImpactNormal.X,
+		HItResult->ImpactNormal.Y, HItResult->ImpactNormal.Z)) ;
 }
